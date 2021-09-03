43461e0080cb9302d6b0131a5daba241d08ccff1
Ezell <lynellf@gmail.com>
2021-09-03 08:31:25 -0500

    dont bundle external dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+// import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 
 export default {
@@ -11,7 +11,7 @@ export default {
     exports: "auto",
   },
   plugins: [
-    commonjs(),
+    // commonjs(),
     typescript({ tsconfig: "./tsconfig.json" }),
     nodeResolve(),
     json(),
